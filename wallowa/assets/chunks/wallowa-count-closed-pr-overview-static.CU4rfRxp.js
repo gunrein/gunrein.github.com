754d582abb2813a6a6a6a600a3771754e5a026a0
Greg Unrein <greg@unre.in>
2024-05-21 20:44:39 -0700

    Fix path issue with Wallow website build

--- wallowa/assets/chunks/wallowa-count-closed-pr-overview-static.CU4rfRxp.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/wallowa-merged-pr-overview-static.DhR20qan.png",a="/assets/wallowa-count-closed-pr-overview-static.jpZuKdb1.png";export{s as _,a};
---
+++ wallowa/assets/chunks/wallowa-count-closed-pr-overview-static.Cb_Rh0JZ.js
@@ -0,0 +1,1 @@
+const a="/wallowa/assets/wallowa-merged-pr-overview-static.DhR20qan.png",s="/wallowa/assets/wallowa-count-closed-pr-overview-static.jpZuKdb1.png";export{a as _,s as a};
